11717657f12296a15e9d95cd71d895bd342ee302
Leonardo Araújo <leonardoluciano1@gmail.com>
2021-11-23 17:51:11 -0300

    DIO's challenge to create Git api

---
+++ src/components/header/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import * as S from './styled'
+import useGithub from "../../hooks/github-hooks";
+
+
+const Header = () => {
+    const { getUser } = useGithub();
+    const [usernameForSearch, setUsernameForSearch] = useState();
+
+const submitGetUser = () =>{
+    if (!usernameForSearch) return;
+    return getUser(usernameForSearch);
+}
+    return(
+        <header>
+            <S.Wrapper>
+                <input type="text" 
+                placeholder="Digite aqui o username a pesquisar" 
+                onChange={(Event) => setUsernameForSearch(Event.target.value)}
+                />
+                <button type="submit" onClick={submitGetUser}>
+                    <span>BUSCAR</span>
+                </button>
+            </S.Wrapper>
+
+        </header>
+
+    )
+}
+
+export default Header;
---
+++ src/components/no-search/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from './styled';
+
+const NoSearch = () => {
+    return (
+        <S.Wrapper>Nenhum Usuário Encontrado</S.Wrapper>
+    )
+}
+
+export default NoSearch;
